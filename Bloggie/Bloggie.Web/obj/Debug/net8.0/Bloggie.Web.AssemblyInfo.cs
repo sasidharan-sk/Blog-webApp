2fd8b8b3f34bd3aff50689adc627a97191332629
sasidharan-sk <sasidharanhc450@gmail.com>
2024-03-16 20:37:19 +0530

    Injected DI

--- Bloggie/Bloggie.Web/obj/Debug/net8.0/Bloggie.Web.AssemblyInfo.cs
+++ Bloggie/Bloggie.Web/obj/Debug/net8.0/Bloggie.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca226256017889818933ca9681c74b2b808a143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+068e66645c5e93b8ad3dd8c3484b5b80d40a750b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bloggie.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
